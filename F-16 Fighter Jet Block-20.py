9efc8043ac654766ecc1221affedc06ecf5756a5
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-01 06:00:10 +0500

    Create F-16 Fighter Jet Block-20.py

---
+++ F-16 Fighter Jet Block-20.py
@@ -0,0 +1,8 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+'%#o' % 16, '%o' % 16
+('0o20', '20')
+format(16, '#o'), format(16, 'o')
+('0o20', '20')
+f'{16:#o}', f'{16:o}'
+('0o20', '20')
